e6c8d6c790d0e15d606d0b03532d14cd9387bdd6
renacava <renanocava@gmail.com>
2024-10-02 14:27:57 +0200

    setup 8 sfx channels, play sound on oldest channel

--- audio.lisp
+++ audio.lisp
@@ -1,6 +1,7 @@
 (in-package #:magical-rendering)
 
 (defparameter *audio-initialised?* nil)
+(defparameter *n-sound-channels* 8)
 (defparameter *loaded-sounds* (make-hash-table :test #'equal))
 (defparameter *loaded-songs* (make-hash-table :test #'equal))
 
@@ -9,8 +10,8 @@
              (not *audio-initialised?*))
     (setf *audio-initialised?* t)
     (sdl2-mixer:init)
-    (sdl2-mixer:open-audio 22050 :s16sys 1 1024)
-    (sdl2-mixer:allocate-channels 1)
+    (sdl2-mixer:open-audio 22050 :s16sys *n-sound-channels* 1024)
+    (sdl2-mixer:allocate-channels *n-sound-channels*)
     t))
 
 (defun shutdown-audio ()
@@ -32,6 +33,10 @@
     (sdl2-mixer:quit)
     (setf *audio-initialised?* nil)))
 
+(let ((previous-channel 0))
+  (defun get-oldest-sound-channel ()
+    (setf previous-channel (mod (1+ previous-channel) *n-sound-channels*))))
+
 (defun load-sound (filename)
   (when *audio-initialised?*
     (or (gethash filename *loaded-sounds*)
@@ -43,7 +48,7 @@
 (defun play-sound (filename)
   (when filename
     (ignore-errors
-     (sdl2-mixer:play-channel 0 (load-sound filename) 0))))
+     (sdl2-mixer:play-channel (get-oldest-sound-channel) (load-sound filename) 0))))
 
 (defun load-song (filename)
   (when *audio-initialised?*
@@ -60,3 +65,4 @@
 
 (defun stop-song ()
   (sdl2-mixer:halt-music))
+
