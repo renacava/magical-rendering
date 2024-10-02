9dc135ea8cfed0b32db615d33ffc5c9323e0091a
renacava <renanocava@gmail.com>
2024-10-02 12:16:58 +0200

    setup main loop stuff; export #'exit

--- main.lisp
+++ main.lisp
@@ -1,18 +1,46 @@
 (in-package #:magical-rendering)
 
-(defun main ()
-  (cepl:repl)
-  (loop (livesupport:continuable (funcall #'render-func))))
+(defparameter *running?* nil)
+(defparameter *main-thread-name* "magical-rendering-main-thread")
+(defparameter *main-thread* nil)
+
+(defun main (&optional idle-func)
+  (setf *running?* t)
+  (unless *main-thread*
+    (setf *main-thread*
+          (bt:make-thread
+           (lambda ()
+             (loop
+               (if *running?*
+                   (livesupport:continuable
+                    (try-open-window)
+                    (when (functionp idle-func)
+                      (funcall idle-func))
+                    (funcall #'render-func))
+                   (progn
+                     (try-close-window)
+                     (sleep 0.1)))))
+           :name *main-thread-name*))))
+
+(defun exit ()
+  (setf *running?* nil))
+
+(let ((window-open? nil))
+  (defun try-open-window ()
+    (unless window-open?
+      (setf window-open? t)
+      (cepl:repl)))
+
+  (defun try-close-window ()
+    (unless (or (cepl.lifecycle:shutting-down-p)
+                (cepl.lifecycle:uninitialized-p)
+                (not window-open?))
+      (cepl:quit)
+      (setf window-open? nil))))
 
 (defun render-func ()
   (livesupport:update-repl-link)
-  (step-host)
-  (sleep 0.0001)
-)
+  (step-host))
 
 (defun now ()
-  (float (/ (get-internal-real-time) 1000)))
-
-(defun debug-log (message &rest args)
-  (apply #'format t message args)
-  (finish-output))
+  (float (/ (get-internal-real-time) internal-time-units-per-second)))
--- package.lisp
+++ package.lisp
@@ -3,4 +3,5 @@
 (defpackage #:magical-rendering
   (:use #:cl #:cepl #:cepl.sdl2 #:rtg-math)
   (:export
-   #:main))
+   #:main
+   #:exit))
